11a2d2dc2a6d7b36f9543e16ea3b2d7b46c4b536
Naing Lin Aung <nainglinaung91@gmail.com>
2015-04-09 10:40:14 +0630

    toggle completed

--- SidebarMenu/NewsTableViewController.swift
+++ SidebarMenu/NewsTableViewController.swift
@@ -13,6 +13,12 @@ class NewsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
